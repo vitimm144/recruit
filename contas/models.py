d12b8f06ab07a733bcb1efd383ebfaeec12e229e
victor <vitimm144@gmail.com>
2016-08-28 14:48:22 -0300

    Criado model Contratante.

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+TIPOS_DE_USUARIOS=(
+    ('SE', 'Recrutador Senior'),
+    ('MA', 'Recrutador Master'),
+    ('RE', 'Recrutador'),
+    ('CA', 'Candidato'),
+    ('CO', 'Contratante'),
+)
+
+
+class Usuario(AbstractBaseUser):
+    tipo = models.CharField(
+        max_length=2,
+        choices=TIPOS_DE_USUARIOS
+    )
+    nome = models.CharField(max_length=150, verbose_name='Nome')
+    email = models.EmailField()
+    celular = models.CharField(max_length=30, verbose_name='Celular')
+    skype = models.CharField(max_length=255, verbose_name='Skype')
+    linkedin = models.CharField(max_length=255, verbose_name='LinkedIN')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        abstract = True
+
+
+class Contratante(Usuario):
+    email_profissional = models.EmailField(null=True, verbose_name='Email Profissional')
+    telefone_empresarial = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name='Telefone Empresarial'
+    )
+    empresa = models.ForeignKey('core.Empresa')
+    cargo = models.CharField(max_length=255)
+
+
+# class Recrutador(Usuario):
--- contas/tests.py
+++ contas/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from contas.models import Contratante
 
-# Create your tests here.
+
+# class ContasModelTestCase(TestCase):
+#
+#     def test_contratante_model(self):
+#         contratante = Contratante.objects.create(
+#             nome='Victor',
+#             email='vitimm144@gmail.com',
+#             tipo='CO',
+#             password='123',
+#             celular='988336439',
+#             skype='qwwee',
+#             linkedin='dsdf',
+#
+#         )
